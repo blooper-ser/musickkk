3163f032295ef3e0b71d74b66ec1bf7a1aa0eb5b
blooperfx <randomstatus2006@gmail.com>
2026-02-25 22:26:09 +0530

    changesss

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../models/User");
+const User = require("../models/user");
 const verifyToken = require("../middleware/verifyToken");
 const verifyAdmin = require("../middleware/verifyAdmin");
 
